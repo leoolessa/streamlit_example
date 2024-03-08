b5f9c487473c356a49e47784595ff0c568e762d2
leoolessa <70817999+leoolessa@users.noreply.github.com>
2024-03-08 18:40:04 +0100

    done

--- pages/_1_.data.py
+++ pages/_1_.data.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import pandas as pd
 import src.manage_data as cleaning
 
 df = cleaning.load_dataframe()
 
 # df = load_daatframe
 st.write('Adventure time data')
-st.Dataframe(df)
+st.dataframe(df)
--- pages/_3_.Maps.py
+++ pages/_3_.Maps.py
@@ -1,28 +1,59 @@
 import streamlit as st
-#from streamlit_folium import folium_static
+from streamlit_folium import folium_static
 import streamlit.components.v1 as components
 import codecs
 import folium
 import requests
-# pip install streamlit-folium? 
+import time
 
-ad = st.text_input("Enter an address: ")
-try :
-    data = requests.get(f'https://geocode.xyz/{ad}?json=1')
-except:
-    latlon = []
+# 1. Declare a default location
+st.write("""
+### Folium map
+""")
 
-
-icon_ = folium.Icon(color = "blue",
-                    prefix = "fa",
-                    icon = "rocket",
-                    icon_color = "black"
-                    )
+default_value_goes_here = "Carrer de Pamplona 96 08018 Barcelona"
+user_input = st.text_input("Enter an adress: ", default_value_goes_here)
 
 latlon = [41.3977737, 2.1904561]
-the_data = {"location": latlon, "tooltip": "Ironhack", "icon": icon_}
-marker = folium.Marker(**the_data)
 
-map_1 = folium.Map(location = latlon, zoom_start = 15)
-marker.add_to(map_1)
+# 2. Try to get a new one through geocode
+
+
+
+# 3. Plot a map
+
+icon_ = folium.Icon(color="blue",
+            prefix = "fa",
+            icon="rocket",
+            icon_color="black")
+
+try:
+    time.sleep(5)
+    data = requests.get(f"https://geocode.xyz/{user_input}?json=1").json()
+    if data.status_code == 200:
+        latlon = [data["latt"], data["longt"]]
+    else:
+        latlon = [41.3977737, 2.1904561]
+
+except:
+    st.warning('Geocode doesnt want to work', icon="🤷‍♂️")
+
+data_2 = {"location": latlon, "tooltip": "Ironhack", "icon":icon_}
+mark = folium.Marker(**data_2)
+
+map_1 = folium.Map(location = latlon, zoom_start=15)
+mark.add_to(map_1)
+
 folium_static(map_1)
+
+
+# 4. Insert a downloaded HTML map
+
+st.write("""
+### Inserted map with HTML
+""")
+
+f=codecs.open("data/choropleth-spain.html", 'r')
+mapa = f.read()
+
+components.html(mapa,height=550,scrolling=True)
